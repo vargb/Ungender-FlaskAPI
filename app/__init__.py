3edb36803e70115bb5c2f617b26d0698bcee7da1
VGB <9965vgb@gmail.com>
2023-11-17 23:27:56 +0530

    auth csrf is disabled

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,9 @@ srv.config["SQLALCHEMY_DATABASE_URI"] = dbUrl
 srv.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 srv.config['SECRET_KEY'] = "iwannakillmyself"
 srv.config['JWT_TOKEN_LOCATION'] = ['cookies']
-srv.config['JWT_COOKIE_CSRF_PROTECT'] = True
-srv.config['JWT_CSRF_CHECK_FORM'] = True
+srv.config['WTF_CSRF_ENABLED'] = False
+srv.config['JWT_COOKIE_CSRF_PROTECT'] = False
+srv.config['JWT_CSRF_CHECK_FORM'] = False
+
 jwt = JWTManager(srv)
 app=server(srv)
